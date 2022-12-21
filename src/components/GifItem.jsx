510a34694c5a61343655fe2de9d573509e8842f5
Matias <cesarmatias1989@gmail.com>
2022-12-21 20:01:11 -0300

    error en jest

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import PropTypes from 'prop-types';
 
 export const GifItem = ({title, url, id}) => {
 
@@ -10,4 +10,9 @@ export const GifItem = ({title, url, id}) => {
         <p>{title}</p>
     </div>
   )
+}
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
